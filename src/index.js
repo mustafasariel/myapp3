625c9d3e93bca843644bcda1fdf8b0c84cc89536
Mustafa <mustafasariel@gmail.com>
2022-01-02 00:34:45 +0300

    props ve  children örneklendi

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./components/Card";
+import Collapse from "./components/Collapse";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const myArr = [
+  { cardTitle: 'Galatasaray', cardContent: 'merhaba galatasaray', cardFooter: '3 dk önce güncellendi', imageUrl: 'https://picsum.photos/id/2/40/40' },
+  { cardTitle: 'Fener', cardContent: 'merhaba Fener', cardFooter: '3 dk önce güncellendi', imageUrl: 'https://picsum.photos/id/1/40/50' },
+  { cardTitle: 'Beşiktaş', cardContent: 'merhaba beşiktaş', cardFooter: '3 dk önce güncellendi', imageUrl: 'https://picsum.photos/id/3/40/50' }
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <div className="card-group">
+
+        {
+          myArr.map((i) => (<Card cardTitle={i.cardTitle} cardContent={i.cardContent} cardFooter={i.cardFooter} imageUrl={i.imageUrl} />))
+
+        }
+      </div>
+    </div>
+  );
+}
+
+function App2()
+{
+  return (
+    <div className="card-group">
+      
+      <Collapse  href="collapseExample1" name={"Birinci kart"}>
+       <Card cardTitle={myArr[0].cardTitle} cardContent={myArr[0].cardContent} cardFooter={myArr[0].cardFooter} imageUrl={myArr[0].imageUrl} ></Card>
+      </Collapse>
+
+      <Collapse href="collapseExample2" name={"ikinci kart"}>
+       <Card cardTitle={myArr[1].cardTitle} cardContent={myArr[1].cardContent} cardFooter={myArr[1].cardFooter} imageUrl={myArr[1].imageUrl} ></Card>
+      </Collapse>
+      
+      <Collapse href="collapseExample3" name="üçüncü kart">
+       <Card cardTitle={myArr[2].cardTitle} cardContent={myArr[2].cardContent} cardFooter={myArr[2].cardFooter} imageUrl={myArr[2].imageUrl} ></Card>
+      </Collapse>
+    </div>
+  );
+}
+
+//  ReactDOM.render(<App />, document.getElementById("root2"));
+
+ReactDOM.render(<App2 />, document.getElementById("root1"));
